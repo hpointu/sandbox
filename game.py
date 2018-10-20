d6d6905d4f6862e26379a03008aff439220feb55
freddy <freddy.teuma@smartodds.co.uk>
2018-10-20 20:25:16 +0100

    Incorporate target into agent struct

--- game.py
+++ game.py
@@ -2,7 +2,7 @@ import random
 import time
 
 
-CELL_SIZE = 10  # simulate walking distance
+CELL_SIZE = 20  # simulate walking distance
 SWITCH_THRESHOLD = 20
 FOOD_PORTION = 1
 
@@ -13,7 +13,7 @@ def create_world(w, h):
         return seed * 10
 
     grid = [
-        food(random.randint(1, 5))
+        food(random.randint(0, 5))
         for i in range(w * h)
     ]
 
@@ -37,7 +37,7 @@ def cell_index(world, x, y):
 
 
 def create_agent(world, i):
-    return cell_coords(world, i)
+    return cell_coords(world, i), None
 
 
 def cell_neighbours(world, i):
@@ -58,66 +58,98 @@ def cell_neighbours(world, i):
 def find_target(world, agent, switch_threshold=SWITCH_THRESHOLD):
     """ Scan agent surroundings for more food """
     grid, _, _ = world
-    i = cell_index(world, *agent)
+    i = cell_index(world, *agent[0])
     neighs = cell_neighbours(world, i)
     food = grid[i]
-    mod = switch_threshold if food else 0
+    mod = switch_threshold if food > 0 else 0
     surrounding = [(k, grid[k] - mod) for k in neighs]
-    return max([(i, food)] + surrounding,
-               key=lambda s: s[1])
+    target = max(
+        [(i, food)] + surrounding,
+        key=lambda s: s[1]
+    )
+    return agent[0], target[0]
 
 
-def move_agent(world, agent, target, speed=1):
-    x, y = agent
+def move_agent(world, agent, speed=1):
+    (x, y), target = agent
     tx, ty = cell_coords(world, target)
 
     if (x, y) == (tx, ty):
-        return agent, False
+        return (x, y), None
 
     dx = speed if tx > x else -speed
     dy = speed if ty > y else -speed
 
-    return (x + dx, y + dy), True
+    return (x + dx, y + dy), target
 
 
 def eat(world, agent, portion=FOOD_PORTION):
     grid, w, h = world
-    i = cell_index(world, *agent)
+    i = cell_index(world, *agent[0])
     grid[i] -= portion
     return grid, w, h
 
 
+def grow_food(world):
+    grid, w, h = world
+
+    def grow(c):
+        g = 0 if random.random() > 0.01 else 1
+        return c + g
+
+    grid = [grow(c) for c in grid]
+    return grid, w, h
+
+
+def step_world(world):
+    return grow_food(world)
+
+
+def step_agent(world, a):
+    a = find_target(world, a)
+    a = move_agent(world, a)
+    if not a[1]:
+        world = eat(world, a)
+    return world, a
+
+
+# From here it's only to render the game in the terminal
+
 def draw_agent(world, a):
-    x, y = a
+    (x, y), target = a
     cell = cell_index(world, x, y)
-    print("Pos: {:0>2},{:0>2}".format(x, y))
+    t = ""
+    if target:
+        t = "-> {}".format(cell_coords(world, target))
+    print("Pos: {:0>2},{:0>2} {}".format(x, y, t))
     print("cell: {}".format(cell))
 
 
 def draw_world(world):
     grid, w, h = world
-    rows = (' '.join("{:0>2}".format(c)
+    rows = (' '.join("{:0>2d}".format(c)
                      for c in grid[i*w:i*w+w])
             for i in range(h))
     print('\n'.join(rows))
 
 
-def render(world, agent, target):
+def render(world, agents):
     print("---")
-    draw_agent(world, agent)
-    print("Moving to: {}".format(cell_coords(world, target[0])))
+    for agent in agents:
+        draw_agent(world, agent)
     print()
     draw_world(world)
     print()
 
 
 if __name__ == "__main__":
-    g = create_world(10, 10)
-    a = create_agent(g, random.randrange(0, 100))
+    world = create_world(10, 10)
+    agents = [create_agent(world, random.randrange(0, 100)),
+              create_agent(world, random.randrange(0, 100))]
     while True:
-        target = find_target(g, a)
-        a, moved = move_agent(g, a, target[0])
-        if not moved:
-            g = eat(g, a)  # only eat when not moving
-        render(g, a, target)
+        world = step_world(world)
+        for i, a in enumerate(agents):
+            world, agents[i] = step_agent(world, agents[i])
+
+        render(world, agents)
         time.sleep(0.1)
